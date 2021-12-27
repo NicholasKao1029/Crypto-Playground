3179b98c55c2da0fa75e088abee4106f1d1623c8
Nick Kao <kobenkao@gmail.com>
2021-12-27 07:48:29 -0800

    changed config network to kovan

--- erc-20/hardhat.config.js
+++ erc-20/hardhat.config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    rinkeby: {
+    kovan: {
       url: `${process.env.ALCHEMY_URL}`,
       accounts: [`0x${process.env.KOVAN_PRIVATE_KEY}`],
     },
