f80ac91fbb3a184a2a29bf5b47acf8680a27abf8
Nick Kao <kobenkao@gmail.com>
2021-12-27 07:47:43 -0800

    erc-20 token contracted created, deploy script created

--- erc-20/contracts/MetaToken.sol
+++ erc-20/contracts/MetaToken.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MetaToken is ERC20 {
-    uint constant _initial_supply = 100 * (10**18);
-    constructor() ERC20("GoofyGoober", "GG") public {
+
+    uint constant inital_token_supply = 100;
+    uint constant magnitude = (10**18);
+
+
+    uint constant _initial_supply = inital_token_supply * magnitude;
+    constructor() ERC20("MetaToken", "MT") public {
         _mint(msg.sender, _initial_supply);
     }
 }
